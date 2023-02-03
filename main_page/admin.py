c2ceecfc7b9a992b2b368a19ae0f37c4ec506826
naasstiia123 <nastabka@gmail.com>
2023-02-04 01:08:04 +0200

    Initial commit

--- main_page/admin.py
+++ main_page/admin.py
@@ -37,3 +37,18 @@ admin.site.register(models.Address)
 class Feedback(admin.ModelAdmin):
     list_display = ['date', 'name', 'product']
     list_filter = ['date', 'product']
+
+
+from django.contrib.contenttypes.models import ContentType
+
+
+class ContentTypeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'app_label']
+    fieldsets = (
+        ('', {
+            'classes': ('',),
+            'fields': ('name', 'app_label')
+        }),
+    )
+
+    admin.site.register(ContentType, ContentTypeAdmin)
