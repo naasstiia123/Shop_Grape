c4638fca5b97afa712a5ac56e85130eb4797f5ea
naasstiia123 <nastabka@gmail.com>
2023-02-03 23:16:12 +0200

    Initial commit

--- main_page/context_processor.py
+++ main_page/context_processor.py
@@ -3,16 +3,16 @@ from .models import Category_good, Phone, Email_adress, Address
 
 def getting_category(request):
     category = Category_good.objects.filter(is_visible=True)
-    return {'category': category}
+    return {'category': category, }
 
 def getting_number(request):
     phone = Phone.objects.filter(is_visible=True)
-    return {'phone': phone}
+    return {'phone': phone, }
 
 def getting_mail(request):
     mails = Email_adress.objects.filter(is_visible=True)
-    return {'mails': mails}
+    return {'mails': mails, }
 
 def getting_address(request):
     location = Address.objects.filter(is_visible=True)
-    return {'location': location}
+    return {'location': location, }
