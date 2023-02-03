731225fe7c8f65d4009b1804a38547cfc729faf9
naasstiia123 <nastabka@gmail.com>
2023-02-03 23:37:34 +0200

    Initial commit

--- Shop_Grape/settings.py
+++ Shop_Grape/settings.py
@@ -32,7 +32,7 @@ DB_PORT = os.environ.get('DB_PORT')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['shop-grape.herokuapp.com']
 
